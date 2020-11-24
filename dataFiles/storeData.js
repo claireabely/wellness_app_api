6510bcd0be546dccc568310be71e85d7f9a56ddc
claireabely <71153552+claireabely@users.noreply.github.com>
2020-11-24 15:20:35 -0500

    heroku

--- dataFiles/storeData.js
+++ dataFiles/storeData.js
@@ -1,9 +1,15 @@
 stores = [{
         store_id: "123",
         storeName: "Wellness Market",
-        imgUrl: "https://i1.wp.com/www.eatthis.com/wp-content/uploads/2018/12/the-golden-mcdonalds-arch.jpg?fit=1200%2C879&ssl=1",
+        imgUrl: "https://images.squarespace-cdn.com/content/v1/5d37375de47a2a0001071628/1602372864723-FQL85BK2024T2EDI9DPJ/ke17ZwdGBToddI8pDm48kLrQnd3tdlQHoi35tdatgzRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpz8CxRD_lte4BsPmEGGAS09NTjnJO9fCHIuJZFekHsS8VbwhKRHeD_fsBnlJq72u68/WebHomeTiles_Nationwide.png?format=500w",
         tags: ["supplements", "herbs", "natural"],
-        menu: [ ],
+        menu: [  { 
+            item_id: 'ba',
+            item_id: 'bb',
+            item_id: 'bc',
+            item_id: 'bd',
+            item_id: 'be',
+            } ],
     },
 
     {
@@ -12,7 +18,13 @@ stores = [{
         imgUrl: "https://d1ralsognjng37.cloudfront.net/3c2c3cf2-143e-42da-9c05-fca34d50cfd3.jpeg",
         tags: ["juice", "smoothies", "wellness bundles"],
         menu: [
-           
+            { 
+                item_id: 'ba',
+                item_id: 'bb',
+                item_id: 'bc',
+                item_id: 'bd',
+                item_id: 'be',
+                }
         ],
     },
 
@@ -21,7 +33,13 @@ stores = [{
         storeName: "Local Delivery",
         imgUrl: "https://images.squarespace-cdn.com/content/v1/5d37375de47a2a0001071628/1602372830113-XGUCGHL6DZ46C2724UB2/ke17ZwdGBToddI8pDm48kLrQnd3tdlQHoi35tdatgzRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpz8CxRD_lte4BsPmEGGAS09NTjnJO9fCHIuJZFekHsS8VbwhKRHeD_fsBnlJq72u68/WebHomeTiles_Local.png?format=500w",
         tags: ["smoothies", "salad", "vegan"],
-        menu: [],
+        menu: [  { 
+            item_id: 'ba',
+            item_id: 'bb',
+            item_id: 'bc',
+            item_id: 'bd',
+            item_id: 'be',
+            }],
     },
 
     {
@@ -30,7 +48,13 @@ stores = [{
         imgUrl: "https://images.squarespace-cdn.com/content/v1/5d37375de47a2a0001071628/1602372864723-FQL85BK2024T2EDI9DPJ/ke17ZwdGBToddI8pDm48kLrQnd3tdlQHoi35tdatgzRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpz8CxRD_lte4BsPmEGGAS09NTjnJO9fCHIuJZFekHsS8VbwhKRHeD_fsBnlJq72u68/WebHomeTiles_Nationwide.png?format=500w",
         rating: 2.5,
         tags: ["fried chicken"],
-        menu: [],
+        menu: [  { 
+            item_id: 'ba',
+            item_id: 'bb',
+            item_id: 'bc',
+            item_id: 'bd',
+            item_id: 'be',
+            }],
     },
     {
         store_id: "789",
@@ -38,7 +62,13 @@ stores = [{
         imgUrl: "https://images.squarespace-cdn.com/content/v1/5d37375de47a2a0001071628/1583763570285-48N2DO0U0J9X60RBCH6E/ke17ZwdGBToddI8pDm48kLBnCuLS4rYL7yVlMx_8oR57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1UeSDz6IyKK09zZ-7q_gpQHSSpVyuY93tgEx5P2GE3RQd71VtgaaASevlyRsadXtEgw/IMG_1487.jpg?format=500w",
         rating: 3.1,
         tags: ["CBD", "health", "health"],
-        menu: [],
+        menu: [  { 
+            item_id: 'ba',
+            item_id: 'bb',
+            item_id: 'bc',
+            item_id: 'bd',
+            item_id: 'be',
+            }],
     },
     
 ];
